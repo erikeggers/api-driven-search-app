f37d8b7871c0012bd0d05fcb7e57937f74be7729
Erik Eggers <em.eggers@gmail.com>
2018-04-15 14:09:52 -0400

    fixing gh-pages

--- js/main.js
+++ js/main.js
@@ -1,24 +1,34 @@
-var movieKeywords;
+let movieKeywords;
 
+//Check if local storage exists
+if (localStorage.getItem('searchValue')) {
+    let storage = localStorage.getItem('searchValue');
+    document.querySelector('#movie-title').value = storage;
+    searchQuery(storage);
+}
+
+//Capture search keywords
 document.querySelector(".movie-search").addEventListener('submit', function(event){
     event.preventDefault();
     movieKeywords = document.querySelector('#movie-title').value;
+    localStorage.setItem('searchValue', movieKeywords);
+    console.log(localStorage.getItem('searchValue'));
     searchQuery(movieKeywords);
 });
 
+//Fun search query
 function searchQuery (keywords) {
-    var url = 'https://api.themoviedb.org/3/search/movie?api_key=7262b9dce7fa1f8eb48117f0179c21ae&query=' + keywords + '&include_adult=false'
-
+    let url = 'https://api.themoviedb.org/3/search/movie?api_key=7262b9dce7fa1f8eb48117f0179c21ae&query=' + keywords + '&include_adult=false'
 
     fetch(url)
         .then(response => response.json())
 
         .then(responseAsJson => {
 
-            var imageURL = 'https://image.tmdb.org/t/p/w1280/'
-            var noPoster = 'https://images.pexels.com/photos/274937/pexels-photo-274937.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-            var movieContainer = document.querySelector('#movie-container');
-            var results = responseAsJson.results;
+            let imageURL = 'https://image.tmdb.org/t/p/w1280/'
+            const noPoster = 'https://images.pexels.com/photos/274937/pexels-photo-274937.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+            const movieContainer = document.querySelector('#movie-container');
+            let results = responseAsJson.results;
 
             if (results.length != 0) {
                 movieContainer.innerHTML="<h2> Results for " + movieKeywords + "</h2>";
@@ -26,7 +36,7 @@ function searchQuery (keywords) {
                 movieContainer.innerHTML="<h2> No results for " + movieKeywords + "</h2>";
             }
             
-            var movie = '<ul>';
+            let movie = '<ul>';
             for (let i = 0; i < results.length; i++) {
 
                 movie += '<li>'
@@ -50,5 +60,3 @@ function searchQuery (keywords) {
             console.log('Error: ', error)
     })
 }
-
-//https://image.tmdb.org/t/p/w1280/
